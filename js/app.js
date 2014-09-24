b4770bfa960240d6b1f75794a437f3c7a8902244
cultrerac <chriscultrera@Chriss-MacBook-Air.local>
2014-09-23 22:18:46 -0400

    Full program
    the whole shebang

---
+++ js/app.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+	$('form input[name="integer"').keydown(function(event) {
+		if (event.which == 13) {
+			event.preventDefault();
+			processInput();
+		}
+	});
+
+	$('form input[value="The Button!"').click(function() {
+		processInput();
+	});
+});
+
+function isPositiveInteger(number) {
+	return number === +number && number === (number|0) && number >= 0/* && number % 1 != 0*/;
+	//return number === +number && number === (number|0) && number >= 0 && number != Math.floor(number);
+	//return number === +number && number === (number|0) && number >= 0 && number.indexOf('.') === -1;
+	//return number === +number && number === (number|0) && number >= 0 && Number.isInteger(number);
+}
+
+function processInput() {
+	$('#result').text('');
+	var integer = parseInt($('form input[name="integer"').val());
+
+	if (isPositiveInteger(integer)) {
+		if (integer === 0) {
+			$('#result').text(integer);
+		} else {
+			$('#result').text(fizzBuzz(integer));
+		}
+	} else {
+		alert('Please enter a valid, positive integer. PLEASE‽');
+		$('form input[name="integer"').val('');
+	}
+}
+
+function fizzBuzz(integer) {
+	var result = "";
+
+	for (var i = 1; i <= integer; i++) {
+		if (i % 3 == 0 && i % 5 == 0) {
+			result += 'FizzBuzz ';
+		} else if (i % 3 == 0) {
+			result += 'Fizz ';
+		} else if (i % 5 == 0) {
+			result += 'Buzz ';
+		} else {
+			result += i + ' ';
+		}
+	}
+
+	return result;
+}
